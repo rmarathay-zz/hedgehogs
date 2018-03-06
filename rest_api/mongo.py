4fc9e791a75d0fe4d49b9af9b95ad9f56017fd83
Ranjit Marathay <rmarathay@esker-23.dynamic.rpi.edu>
2018-03-06 16:43:18 -0500

    adding beginning of rest api

--- rest_api/mongo.py
+++ rest_api/mongo.py
@@ -13,3 +13,8 @@ app.config['MONGO_AUTH_MECHANISM'] = 'SCRAM-SHA-1'
 app.config['MONGO_URI'] = 'mongodb://45.55.48.43:27017/SEC_EDGAR'
 
 mongo = PyMongo(app)
+
+
+@app.route('/')
+def hello():
+	return 'Hello, World!'
