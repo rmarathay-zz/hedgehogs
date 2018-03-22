4326a1ac490964781dcd80b6e4354848d6220dd8
jhines8080 <jhines8080@gmail>
2018-03-22 18:49:01 +0000

    changes to mongo URI

--- rest_api/mongo.py
+++ rest_api/mongo.py
@@ -11,7 +11,7 @@ app.config['MONGO_USERNAME'] = 'rmarathay'
 app.config['MONGO_PASSWORD'] = '1234'
 app.config['MONGO_AUTH_SOURCE'] = 'admin'
 app.config['MONGO_AUTH_MECHANISM'] = 'SCRAM-SHA-1'
-app.config['MONGO_URI'] = 'mongodb://45.55.48.43:27017/SEC_EDGAR'
+app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/SEC_EDGAR'
 
 mongo = PyMongo(app)
 
