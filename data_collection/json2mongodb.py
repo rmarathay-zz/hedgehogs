a2774c48d5183d3ac107eb3ddc00140592630b04
jarod <jaroddanielgeorge@gmail.com>
2018-04-09 15:31:05 +0000

    standardizing fix

--- data_collection/json2mongodb.py
+++ data_collection/json2mongodb.py
@@ -1,11 +1,32 @@
+#-*- coding: utf-8 -*-
+# import os
+# from optparse import OptionParser
+# from pymongo import MongoClient, bulk
+# import json
+# import collections
+# import sys
+
+from import_hedgehogs import *
+
+HOST = '45.55.48.43'
+PORT = 27017
+DB = 'SEC_EDGAR'
 
 
-hedgehogsdata_collection
-Name
-Last Modified
+class OrderedDictWithKeyEscaping(collections.OrderedDict):
+    def __setitem__(self, key, value, dict_setitem=dict.__setitem__):
+        # MongoDB complains when keys contain dots, so we call json.load with
+        # a modified OrderedDict class which escapes dots in keys on the fly
+        key = key.replace('.', '<DOT>')
+        super(OrderedDictWithKeyEscaping, self).__setitem__(key, value)#, dict_setitem=dict.__setitem__)
+        #super(OrderedDictWithKeyEscaping, self).__setitem__
+        #super()
 
-object_pairs_hook
-​
+def save_to_mongodb(input_file_name, collectionID, usernameID, passwordID):
+    with open(input_file_name) as fp:
+        data = fp.read()
+        json_ = json.loads(data, encoding='utf-8', object_pairs_hook=OrderedDictWithKeyEscaping)
+
     client = MongoClient(HOST, PORT, username=usernameID, password=passwordID, authMechanism ='SCRAM-SHA-1')
     # client.admin.authenticate('jgeorge','123',source= 'SEC_EDGAR')
     # print("arguments to function:", input_file_name, usernameID, collectionID)
@@ -44,9 +65,9 @@ def standardize(obj):
     # delete items from list as you go along ones remaining add to db?
     
     for key in obj[0]:
-        ##print(key)
         if key == 'title':
-            continue
+            break
+        print(key)
         newKey = "Document and Entity Information"
         temp = key
         if(newKey!=key):
@@ -75,3 +96,56 @@ def get_collection_name(input_file_name):
     data = dict(data_list[0])
     year = "YEAR"
     ticker = "TICKER"
+    quarter = "QUARTER"
+    try:
+        # year = data.get("Document And Entity Information [Abstract]")
+        # print(year)
+        year = data.get("Document And Entity Information").get("Document Fiscal Year Focus").get("value")
+        quarter = data.get("Document And Entity Information").get("Document Fiscal Period Focus").get("value")
+        ticker = data.get("Document And Entity Information").get("Entity Trading Symbol").get("value")
+    except AttributeError:
+        print("[EXCEPT] Issues with ", input_file_name)
+    
+    
+    # except AttributeError:
+    #     year = data.get("Document And Entity Information").get("Document Fiscal Year Focus").get("value")
+    #     quarter = data.get("Document And Entity Information").get("Document Fiscal Period Focus").get("value")
+    #     try:
+    #         ticker = data.get("Document And Entity Information [Abstract]").get("Entity Trading Symbol").get("value")
+    #     except:
+    #         ticker = data.get("Document And Entity Information [Abstract]").get("Trading Symbol").get("value")
+    # try:
+    #     ticker = data.get("Document And Entity Information [Abstract]").get("Entity Trading Symbol").get("value")
+    # except:
+    #     ticker = data.get("Document And Entity Information [Abstract]").get("Trading Symbol").get("value")
+        
+    # quarter = data.get("Document And Entity Information [Abstract]").get("Document Fiscal Period Focus").get("value")
+    return str(ticker) + "_" + str(year) + "_" + str(quarter)
+    
+def main():
+    cli_parser = OptionParser(
+        usage='usage: %prog <input.json> <username> <password>'
+        )
+    (options, args) = cli_parser.parse_args()
+
+    # Input file checks
+    if len(args) < 2:
+        cli_parser.error("You have to supply 2 arguments, USAGE: .json username")
+    input_file_name = args[0]
+    if not os.path.exists(input_file_name):
+        cli_parser.error("The input file %s you supplied does not exist" % input_file_name)
+    jsonDarulo = getJsonObj(input_file_name)
+    jsonDarulo = standardize(jsonDarulo)
+    json2File(jsonDarulo)
+    
+    # JAROD's FUNCTION
+    collection = get_collection_name(input_file_name)
+    #collection = (sys.argv[1]).strip('.')
+    username = sys.argv[2]
+    password = sys.argv[3]
+    print("Adding to MongoDB...")
+    #save_to_mongodb(input_file_name, collection, username)
+
+if __name__ == "__main__":
+    print("[WARNING] STILL UNDER DEVELOPMENT")
+    main()
