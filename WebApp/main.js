ad4718b33dee8195b4fdd12a162993dfa0b92873
jhines8080 <hinesj5@rpi.edu>
2018-05-03 22:58:02 -0400

    Beginning of web app, basic information displayed

--- WebApp/main.js
+++ WebApp/main.js
@@ -1,37 +1,31 @@
-var app = new Vue({
-	el: '#app',
-	data: {
-		brand: 'Vue Mastery',
-		product: 'Socks',
-		image: 'greensocks.jpg',
-		inStock: true,
-		// inventory: 8
-		details: ["80% cotton", "20% polyester", "Gender-neutral"],
-		variants: [
-			{
-				variantId: 2234,
-				variantColor: "green",
-				variantImage: "greensocks.jpg"
-			},
-			{
-				variantId: 2235,
-				variantColor: "blue",
-				variantImage: "bluesocks.jpg"
-			}
-		],
-		cart: 0
-	},
-	methods: {
-		addToCart: function () {
-			this.cart += 1
-		},
-		updateProduct: function (variantImage) {
-			this.image = variantImage
-		}
-	},
-	computed: {
-		title() {
-			return this.brand + ' ' + this.product
-		}
-	}
-})
+// // import Tabs from 'vue-tabs-component'
+// // import Tab from 'vue-tabs-component'
+// Vue.component('tabs', Tabs);
+// // Vue.component('Tab', Tab);
+    
+// // new Vue({
+// // 	el:'#vue-tabs-component'
+// // })
+
+// new Vue({
+//     el: '#app', 
+//     data: {
+// 		message: 'Hedgehogs'
+// 	},
+
+// })
+new Vue({
+    el: '#app',
+    data: {
+        choice: 'homeActive',
+        image: 'charticon.png'
+    },
+    methods: {
+        makeActive: function(val) {
+            this.choice = val;
+        },
+        isActiveTab: function(val) {
+          return this.choice === val;
+        }
+    }
+});
