045791a571aad054da4400591c281cd87a02d5e2
Corey Boornazian <coreyb220@gmail.com>
2018-10-08 21:24:42 -0400

    Update docker compose for postgres (not working)

--- data_collection/db/connection.py
+++ data_collection/db/connection.py
@@ -6,17 +6,17 @@ def connection():
     :returns a database connection conn and a cursor cur
     """
     print('[LOG] Trying to connect')
-    
+
     conn = None
-    try:
-        # conn = psycopg2.connect("host=206.189.181.163 port=5432 user=rcos password=hedgehogs_rcos dbname=rcos")
-        conn = psycopg2.connect("host=0.0.0.0 port=5432 user=root password=hedgehogs_rcos dbname=rcos")
-        cur = conn.cursor()
-        print('[LOG] Connected!')
-        return conn, cur
-    except:
-        print("[ERROR] Unable to connect. Returning error..")
-        return -1, -1
+    #try:
+        # conn = psycopg2.connect("host=206.189.181.163 port=5432")
+    conn = psycopg2.connect("host=0.0.0.0 port=5432")
+    cur = conn.cursor()
+    print('[LOG] Connected!')
+    return conn, cur
+    #except:
+    print("[ERROR] Unable to connect. Returning error..")
+    return -1, -1
 
 if __name__ == "__main__":
     connection()
