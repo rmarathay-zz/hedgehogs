29533e53ba49459b08467df3934c7efa8b85dc5e
Corey Boornazian <coreyb220@gmail.com>
2018-10-08 22:09:28 -0400

    Uncomment try catch block
    Signed-off-by: Corey Boornazian <coreyb220@gmail.com>

--- data_collection/db/connection.py
+++ data_collection/db/connection.py
@@ -8,15 +8,15 @@ def connection():
     print('[LOG] Trying to connect')
 
     conn = None
-    #try:
-        # conn = psycopg2.connect("host=206.189.181.163 port=5432")
-    conn = psycopg2.connect("host=192.168.99.100 port=5432 user=rcos password=password")
-    cur = conn.cursor()
-    print('[LOG] Connected!')
-    return conn, cur
-    #except:
-    print("[ERROR] Unable to connect. Returning error..")
-    return -1, -1
+    try:
+        #conn = psycopg2.connect("host=206.189.181.163 port=5432")
+        conn = psycopg2.connect("host=192.168.99.100 port=5432 user=rcos password=password")
+        cur = conn.cursor()
+        print('[LOG] Connected!')
+        return conn, cur
+    except:
+        print("[ERROR] Unable to connect. Returning error..")
+        return -1, -1
 
 if __name__ == "__main__":
     connection()
