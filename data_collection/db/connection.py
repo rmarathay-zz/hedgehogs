d5f6256526f093e70046218b3aff3c26e685d9c5
Corey Boornazian <coreyb220@gmail.com>
2018-10-16 15:29:16 -0400

    Update connection.py
    Signed-off-by: Corey Boornazian <coreyb220@gmail.com>

--- data_collection/db/connection.py
+++ data_collection/db/connection.py
@@ -9,15 +9,15 @@ def connection():
     print('[LOG] Trying to connect')
 
     conn = None
-    #try:
-        #conn = psycopg2.connect("host=206.189.181.163 port=5432")
+    try:
+        conn = psycopg2.connect("host=206.189.181.163 port=5432")
     conn = psycopg2.connect(make_string())
     cur = conn.cursor()
     print('[LOG] Connected!')
     return conn, cur
-#except:
-    print("[ERROR] Unable to connect. Returning error..")
-    return -1, -1
+    except:
+        print("[ERROR] Unable to connect. Returning error..")
+        return -1, -1
 
 if __name__ == "__main__":
     connection()
