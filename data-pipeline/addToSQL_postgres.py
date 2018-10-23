64394a57532d0aa5258f4c43b8ce8daf7354fa1b
Corey Boornazian <coreyb220@gmail.com>
2018-10-23 16:59:08 -0400

    Add addToSQL_postgres.py

---
+++ data-pipeline/addToSQL_postgres.py
@@ -0,0 +1,75 @@
+import psycopg2
+import db
+from pandas_datareader import data, wb
+import pandas_datareader.data as web
+import datetime
+
+def makeRow(symbol, f, date_list):
+    ret = []
+    for date in date_list:
+        row = [symbol, date]
+        row.append(float(f.loc[date,"Open"]))
+        row.append(float(f.loc[date,"High"]))
+        row.append(float(f.loc[date,"Low"]))
+        row.append(float(f.loc[date,"Close"]))
+        row.append(int(f.loc[date,"Volume"]))
+        ret.append(row)
+    return ret
+
+if __name__ == "__main__":
+
+    start = datetime.datetime(2017,1,1)
+    #watchlist = set(["AAPL", 'AMD', 'AMZN', 'ATVI', 'BABA', 'BAC', 'BLK', 'BWLD', 'CELG', 'CRM', 'F', 'FB','GOOG','IBM','INTC','JPM','KORS','LMT','LULU','MOMO','MSFT','MU','NFLX','NKE','NVDA','PCLN','PFE','RTN','SBUX','SHOP','SNAP','SPY','SQ','TAP','TSLA','ULTA','V','VOO','VOOG','VXX'])
+    watchlist = set(['AAPL', 'NFLX', 'BABA'])
+    curr = 1
+    tot = len(watchlist)
+    biglist = []
+
+    for symbol in watchlist:
+        connected = False
+        i = 1
+        while not connected and i < 4:
+            try:
+                f = web.DataReader(symbol.lower(), 'yahoo', start)
+                print("connected ",symbol,'(',curr,' of ',tot,')')
+                curr += 1
+                connected = True
+            except Exception as e:
+
+                print("couldn't connect to data for: ",symbol, ' (Attempt number ', i,')',sep='')
+                i += 1
+                continue
+
+        l = []
+        for index in f.index:
+            l.append(index.strftime('%Y-%m-%d'))
+
+        attempts_sql = 1
+        insql = False
+        while not insql and attempts_sql < 4:
+            try:
+                results = makeRow(symbol, f, l)
+                biglist.extend(results)
+                print(results)
+                insql = True
+            except Exception as e:
+                print("couldn't add ", symbol, ' to mysql (Attempt number ',attempts_sql,')',sep='')
+                attempts_sql += 1
+                continue
+
+    #SQL
+'''
+    db = MySQLdb.connect(host='localhost',user='root',passwd='roott',db='stocks')
+    c = db.cursor()
+    #c.execute("""truncate data""")
+    db.commit()
+    for row in biglist:
+        try:
+'''
+            #c.execute("""INSERT into big_data (primary_key, symbol, date, open, high, low, close, volume) values (0, %s, %s, %s, %s, %s, %s, %s)""",(row[0],row[1],row[2],row[3],row[4],row[5],row[6],))
+'''
+            db.commit()
+        except:
+            db.rollback()
+    db.close()
+'''
---
+++ data-pipeline/db/config.py
@@ -0,0 +1,13 @@
+POSTGRES_USER = "rcos"
+POSTGRES_PASSWORD = "hedgehogs_rcos"
+DBNAME = "rcos"
+
+# Use this host to connect to the remote PostgreSQL server
+#HOST = "206.189.181.163"
+HOST = "192.168.99.100"
+PORT = "5432"
+
+def make_conn_string():
+    ret = "host={} port={} user={} password={} dbname={}"\
+    .format(HOST, PORT, POSTGRES_USER, POSTGRES_PASSWORD, DBNAME)
+    return ret
