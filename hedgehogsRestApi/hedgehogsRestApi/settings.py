d68cc908b9d8a6c94161418af5e4bd4a317a7e24
dylanmoody <moodyd2@rpi.edu>
2018-11-04 02:33:54 -0500

    got the templates for the base_layout and homepage working together next: create bar modeled after the one in the wireframe inside of base_layout so that it can be extended onto all pages that get made

--- hedgehogsRestApi/hedgehogsRestApi/settings.py
+++ hedgehogsRestApi/hedgehogsRestApi/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'hedgehogsRestApi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"templates")  ],
+        'DIRS': ["templates"  ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
