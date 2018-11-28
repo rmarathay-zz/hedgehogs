81825b38d9e405a60797f6928b9f849ac16a2858
Peter Gravelin <pygravelin@gmail.com>
2018-11-27 21:48:56 -0500

    added oauth login

--- hedgehogsRestApi/hedgehogsRestApi/urls.py
+++ hedgehogsRestApi/hedgehogsRestApi/urls.py
@@ -14,17 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from . import views
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.homepage, name = "homepage" ),
     path('about/', include('about.urls', namespace='about')),
     path('tools/', include('tools.urls', namespace='tools')),
-    path('userauth/', include('userAuthentication.urls', namespace='userauth')),
-    path('', views.homepage, name = "homepage" ),
+    # path('userauth/', include('userAuthentication.urls', namespace='userauth')),
+
+    path('login/', LoginView.as_view(template_name='userauth/login.html'), name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('oauth/', include('social_django.urls', namespace='social')),
+    path('admin/', admin.site.urls),
 ]
--- hedgehogsRestApi/hedgehogsRestApi/views.py
+++ hedgehogsRestApi/hedgehogsRestApi/views.py
@@ -1,11 +1,45 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm, UserCreationForm
+from django.contrib.auth import update_session_auth_hash, login, authenticate
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
+from social_django.models import UserSocialAuth
 import os
 
 BASE_DIR = os.path.realpath('.')
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data.get('username'),
+                password=form.cleaned_data.get('password1')
+            )
+            login(request, user)
+            return redirect('homepage')
+    else:
+        form = UserCreationForm()
+    return render(request, 'userauth/signup.html', {'form': form})
+
+@login_required
 def homepage(request):
 	return render(request, BASE_DIR+'/templates/homepage/homepage.html')
 
+@login_required
+def logout(request):
+    user = request.user
+
+    try:
+        github_login = user.social_auth.get(provider='github')
+    except UserSocialAuth.DoesNotExist:
+        github_login = None
+
+    can_disconnect = (user.social_auth.count() > 1 or user.has_usable_password())
+
+    return render(request, 'userauth/logout.html', {
+        'github_login': github_login,
+        'can_disconnect': can_disconnect
+    })
