77b120bca1a01715ea47f0f07ff291c461d2b150
Peter Gravelin <36561941+pgravelin@users.noreply.github.com>
2018-11-30 15:36:38 -0500

    Update urls.py

--- hedgehogsRestApi/userAuthentication/urls.py
+++ hedgehogsRestApi/userAuthentication/urls.py
@@ -8,10 +8,5 @@ from django.urls import path, include
 app_name = 'userAuthentication'
 
 urlpatterns = [
-<<<<<<< HEAD
 
-=======
-	path('', views.userauthpage, name=  "userauthpage"),
-	path('accounts/', include('django.contrib.auth.urls')),
->>>>>>> e058e2784e23a62453c11f3dd2c6529c11aae0ae
 ]
