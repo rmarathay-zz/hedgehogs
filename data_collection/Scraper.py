c2266358088d27ef11c5efe9f480d064f465a2b9
markc373 <markchin373@gmail.com>
2019-02-20 17:55:41 -0500

    Finished scraper, fill companies.txt with companies

--- data_collection/Scraper.py
+++ data_collection/Scraper.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import json
 import psycopg2
+from psycopg2.extensions import AsIs
 from datetime import datetime
 def get_company_info(symbol):
     url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ symbol + "&apikey=1BGCK5RG5D5CQCAJ&datatype=json"
@@ -52,20 +53,23 @@ def read_input(cur):
         dates = []
         for entry in data['Time Series (Daily)']:
             dates.append(entry)
-        dates.sort(key = lambda date: datetime.strptime(date, '%Y-%m-%d'))
-        temp = ("CREATE TABLE %s(date TIMESTAMP, low double precision, high double precision, volume double precision, close double precision, open double precision);")
+        #sort by date
+        dates.sort(key = lambda date: datetime.strptime(date, '%Y-%m-%d')) 
+        #SQL command setup to create table
+        temp = ("CREATE TABLE %s(date TIMESTAMP, low double precision, high double precision, volume double precision, close double precision, open double precision);" % symbol)
+        #execute SQL cmd, with symbol replacing %s
+        cur.execute(temp)
 
-        cur.execute(temp, symbol)
-        insert_st = ("INSERT INTO %s(date,low,high,volume,close,open) VALUES (%s, %f, %f, %f, %f, %f)")
         for date in dates:
-            temp_info = (date, 
+            insert = ("INSERT INTO {}(date,low,high,volume,close,open) VALUES ('{}', {}, {}, {}, {}, {})"
+            .format(symbol, date, 
                         data['Time Series (Daily)'][str(date)]['3. low'],
                         data['Time Series (Daily)'][str(date)]['2. high'],
                         data['Time Series (Daily)'][str(date)]['5. volume'],
                         data['Time Series (Daily)'][str(date)]['4. close'],
-                        data['Time Series (Daily)'][str(date)]['1. open'])
-            cur.execute(insert_st, temp_info)
-
+                        data['Time Series (Daily)'][str(date)]['1. open']))
+            cur.execute(insert)
+        
         #for date in dates:
          #   print(date, ": closed at",data['Time Series (Daily)'][str(date)]['4. close'])
  
