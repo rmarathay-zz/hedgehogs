9a93bd61d4be1170d01a86eb3faeb87500771ebb
christurgeon <turgeonchris3@outlook.com>
2019-03-30 14:25:19 -0400

    Revised test file
    Added accessor to return median value
    
    Added error checking for MAs
    
    Added stuff
    
    added test file
    
    Revised test file

--- data_collection/TestDataProcessing.py
+++ data_collection/TestDataProcessing.py
@@ -1,6 +1,13 @@
 import pytest
 from DataProcessing import StockData
 
+"""
+IMPLEMENT A TEST FILE TO TEST THE FUNCTIONS
+IN DataProcessing.py this should develop
+fake data and and ensure that the calculations
+are correct and that the data types are correct
+"""
+
 def SetUp():
     # Single entry data set
     ticker2 = "TIC2"
